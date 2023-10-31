7ba9567c14fd99870ba722d93075536b01d6a2c0
Dominic Muscatella <dominic.muscatella@Dominic-Muscatellas-MacBook-Pro.local>
2023-10-31 17:00:21 -0500

    modifying jobs 15

--- job.py
+++ job.py
@@ -14,11 +14,10 @@ with open('file.html', 'w') as f:
     # Read remote pdf into a list of DataFrame
     dfs = tabula.read_pdf("https://www.porterco.org/DocumentCenter/View/16019/3-2023-Primary-Election-OFFICIAL-Summary-Results", pages='all', relative_columns=True, columns=[5, 25, 50, 70 ], guess=False,)
     print('LEN', len(dfs))
+    pd.set_option('display.max_columns', None)
     print(dfs[0], dfs[1])
 
-    fig, ax = plt.subplots()
-    ax.bar(symbols[:5], lot_size[:5])
-    fig.savefig('my_plot.png')
-
     html = "<img src='my_plot.png'/>"
+    for df in dfs:
+        html = html + df.to_html()
     f.write(f"{html}")
