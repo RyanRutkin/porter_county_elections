43a20ef1bc6228005295c5789f0ee242621496f2
Dominic Muscatella <dominic.muscatella@Dominic-Muscatellas-MacBook-Pro.local>
2023-10-31 17:38:22 -0500

    modifying jobs 16

--- job.py
+++ job.py
@@ -19,6 +19,9 @@ with open('file.html', 'w') as f:
 
     html = "<img src='my_plot.png'/>"
     for df in dfs:
-        df2 = df.dropna()
-        html = html + df2.to_html()
+        df2 = df.drop(index=0, axis=1)
+        print('INNER')
+        print(df2)
+        df3 = df2.dropna()
+        html = html + df3.to_html()
     f.write(f"{html}")
