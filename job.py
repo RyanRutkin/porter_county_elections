28a785d5a6bc06b1afe5d9dfd64157e31e023ee9
Dominic Muscatella <dominic.muscatella@Dominic-Muscatellas-MacBook-Pro.local>
2023-10-31 16:15:02 -0500

    modifying jobs 8

--- job.py
+++ job.py
@@ -3,10 +3,17 @@ import requests
 import pandas as pd
 import matplotlib.pyplot as plt
 import io
+import tabula
 
 with open('file.html', 'w') as f:
-    url = "https://pkgstore.datahub.io/core/nasdaq-listings/nasdaq-listed_csv/data/7665719fb51081ba0bd834fde71ce822/nasdaq-listed_csv.csv"
-    s = requests.get(url).content
+    # url = "https://pkgstore.datahub.io/core/nasdaq-listings/nasdaq-listed_csv/data/7665719fb51081ba0bd834fde71ce822/nasdaq-listed_csv.csv"
+    # s = requests.get(url).content
+
+    
+
+    # Read remote pdf into a list of DataFrame
+    dfs = tabula.read_pdf("https://www.porterco.org/DocumentCenter/View/16019/3-2023-Primary-Election-OFFICIAL-Summary-Results")
+    print(dfs[0], dfs[1])
     companies = pd.read_csv(io.StringIO(s.decode('utf-8')))
     lot_size = companies['Round Lot Size'].tolist()
     symbols = companies['Symbol'].tolist()
