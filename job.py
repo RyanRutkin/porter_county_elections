91e94188e50dd4ae67c4456d3c38333d1419233b
Dominic Muscatella <dominic.muscatella@Dominic-Muscatellas-MacBook-Pro.local>
2023-10-31 15:41:02 -0500

    modifying jobs 3

--- job.py
+++ job.py
@@ -1,5 +1,19 @@
 from datetime import datetime
+import requests
+import pandas as pd
+import matplotlib.pyplot as plt
+import io
 
-with open('file.txt', 'w') as f:
-    f.write(f"JOB DONE {datetime.now()}")
-    print('FILE_NAME', f.name)
+with open('file.html', 'w') as f:
+    url = "https://pkgstore.datahub.io/core/nasdaq-listings/nasdaq-listed_csv/data/7665719fb51081ba0bd834fde71ce822/nasdaq-listed_csv.csv"
+    s = requests.get(url).content
+    companies = pd.read_csv(io.StringIO(s.decode('utf-8')))
+    lot_size = companies['Round Lot Size'].tolist()
+    symbols = companies['Symbol'].tolist()
+
+    fig, ax = plt.subplots()
+    fig.plot.bar(symbols, lot_size)
+    fig.savefig('my_plot.png')
+
+    html = "<img src='my_plot.png'/>"
+    f.write(f"{html}")
