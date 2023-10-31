08cae30b6a203934c140c1834e7a9db92ab117bd
Dominic Muscatella <dominic.muscatella@Dominic-Muscatellas-MacBook-Pro.local>
2023-10-31 16:54:49 -0500

    modifying jobs 14

--- job.py
+++ job.py
@@ -12,7 +12,7 @@ with open('file.html', 'w') as f:
     
 
     # Read remote pdf into a list of DataFrame
-    dfs = tabula.read_pdf("https://www.porterco.org/DocumentCenter/View/16019/3-2023-Primary-Election-OFFICIAL-Summary-Results", pages='all', relative_columns=True, columns=[10, 15, 50, 70 ], guess=False,)
+    dfs = tabula.read_pdf("https://www.porterco.org/DocumentCenter/View/16019/3-2023-Primary-Election-OFFICIAL-Summary-Results", pages='all', relative_columns=True, columns=[5, 25, 50, 70 ], guess=False,)
     print('LEN', len(dfs))
     print(dfs[0], dfs[1])
 
