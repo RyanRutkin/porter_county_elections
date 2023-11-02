c0f28dad423390fb5c2a4a795c308fe1cbcda30b
rutkin.ryan@gmail.com <rutkin.ryan@gmail.com>
2023-11-01 23:44:38 -0500

    Forcing the data directory to exist

--- job/main.py
+++ job/main.py
@@ -2,7 +2,7 @@ from pypdf import PdfReader
 from io import BytesIO
 import urllib.request
 
-with urllib.request.urlopen('https://www.porterco.org/DocumentCenter/View/15171/3-2022-General-Election-OFFICIAL-Summary') as response, open('/app/data/results.txt', 'w') as out_file:
+with urllib.request.urlopen('https://www.porterco.org/DocumentCenter/View/15171/3-2022-General-Election-OFFICIAL-Summary') as response, open('./app/data/results.txt', 'w') as out_file:
     data = response.read()
     pdfFile = PdfReader(BytesIO(data))
     output = ''
